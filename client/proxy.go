f95037de81575d65ed968dbeb2630c61944a1d9c
mavensingh <kuldeep.avsar@gmail.com>
2020-05-21 11:40:25 +0530

    visiting each url

---
+++ client/proxy.go
@@ -0,0 +1,19 @@
+package client
+
+import "net/http"
+
+func Request(searchURL string) (*http.Response, error) {
+
+	baseClient := &http.Client{}
+
+	req, _ := http.NewRequest("GET", searchURL, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+
+	res, err := baseClient.Do(req)
+
+	if err != nil {
+		return nil, err
+	} else {
+		return res, nil
+	}
+}
