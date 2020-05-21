f439831916417d2c43ac8c313c26cc157b034247
mavensingh <kuldeep.avsar@gmail.com>
2020-05-21 11:40:06 +0530

    made function reusable

--- google/search.go
+++ google/search.go
@@ -1,23 +1,14 @@
 package google
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/googlesearch/client"
+	"github.com/googlesearch/result"
 )
-
-type GoogleResult struct {
-	ResultRank  int
-	ResultURL   string
-	ResultTitle string
-	ResultDesc  string
-}
 
 var googleDomains = map[string]string{
 	"com": "https://www.google.com/search?q=",
@@ -35,99 +26,18 @@ func buildGoogleUrl(searchTerm string, countryCode string) string {
 		return fmt.Sprintf("%s%s&num=100&hl=%s", googleDomains["com"], searchTerm, "en")
 	}
 }
-
-func googleRequest(searchURL string) (*http.Response, error) {
-
-	baseClient := &http.Client{}
-
-	req, _ := http.NewRequest("GET", searchURL, nil)
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-
-	res, err := baseClient.Do(req)
-
-	if err != nil {
-		return nil, err
-	} else {
-		return res, nil
-	}
-}
-
-func googleResultParser(response *http.Response) ([]GoogleResult, error) {
-	doc, err := goquery.NewDocumentFromResponse(response)
-	if err != nil {
-		return nil, err
-	}
-
-	results := []GoogleResult{}
-	sel := doc.Find("div.g")
-	rank := 1
-	for i := range sel.Nodes {
-		item := sel.Eq(i)
-		linkTag := item.Find("a")
-		link, _ := linkTag.Attr("href")
-		titleTag := item.Find("h3")
-		descTag := item.Find("span.st")
-		desc := descTag.Text()
-		title := titleTag.Text()
-		link = strings.Trim(link, " ")
-		if link != "" && link != "#" {
-			result := GoogleResult{
-				rank,
-				link,
-				title,
-				desc,
-			}
-			results = append(results, result)
-			rank += 1
-		}
-	}
-	return results, err
-}
-
-func GoogleScrape(searchTerm string, countryCode string) ([]GoogleResult, error) {
+func GoogleScrape(searchTerm string, countryCode string) ([]result.Result, error) {
 	googleUrl := buildGoogleUrl(searchTerm, countryCode)
-	res, err := googleRequest(googleUrl)
+	res, err := client.Request(googleUrl)
 	if err != nil {
 		return nil, err
 	}
-	scrapes, err := googleResultParser(res)
+	scrapes, err := result.ResultParser(res, "div.g", "a", "h3", "span.st")
 	if err != nil {
 		return nil, err
 	} else {
 		return scrapes, nil
 	}
-}
-
-// readAll reads from r until an error or EOF and returns the data it read
-// from the internal buffer allocated with a specified capacity.
-func readAll(r io.Reader, capacity int64) (b []byte, err error) {
-	var buf bytes.Buffer
-	// If the buffer overflows, we will get bytes.ErrTooLarge.
-	// Return that as an error. Any other panic remains.
-	defer func() {
-		e := recover()
-		if e == nil {
-			return
-		}
-		if panicErr, ok := e.(error); ok && panicErr == bytes.ErrTooLarge {
-			err = panicErr
-		} else {
-			panic(e)
-		}
-	}()
-	if int64(int(capacity)) == capacity {
-		buf.Grow(int(capacity))
-	}
-	_, err = buf.ReadFrom(r)
-	return buf.Bytes(), err
-}
-
-// ReadAll reads from r until an error or EOF and returns the data it read.
-// A successful call returns err == nil, not err == EOF. Because ReadAll is
-// defined to read from src until EOF, it does not treat an EOF from Read
-// as an error to be reported.
-func ReadAllResponse(r io.Reader) ([]byte, error) {
-	return readAll(r, bytes.MinRead)
 }
 
 func GoogleSearch(query, countryCode string) {
