576bd26a1606409592ba9b287899eb1d5ac9a85d
mavensingh <kuldeep.avsar@gmail.com>
2020-06-01 20:22:04 +0530

    added request

--- client/proxy.go
+++ client/proxy.go
@@ -1,19 +1,27 @@
 package client
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 func Request(searchURL string) (*http.Response, error) {
-
-	baseClient := &http.Client{}
-
-	req, _ := http.NewRequest("GET", searchURL, nil)
+	tr := &http.Transport{
+		MaxIdleConns:        10,
+		MaxIdleConnsPerHost: 10,
+		IdleConnTimeout:     30 * time.Second,
+		DisableCompression:  true,
+	}
+	client := &http.Client{Transport: tr}
+	req, err := http.NewRequest("GET", searchURL, nil)
+	if err != nil {
+		return nil, err
+	}
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-
-	res, err := baseClient.Do(req)
-
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	} else {
-		return res, nil
+		return resp, nil
 	}
 }
