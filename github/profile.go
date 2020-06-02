cbae47cd6fd23ad021a13170a314eccc5ab4c4e2
mavensingh <kuldeep.avsar@gmail.com>
2020-06-02 13:05:40 +0530

    added function

--- github/profile.go
+++ github/profile.go
@@ -6,13 +6,11 @@ import (
 )
 
 func Profile(profile_link string) {
-	// fmt.Println(profile_link)
 	repo_to_profile := strings.Split(profile_link, "/")
 	protocol := ReplaceSpace(repo_to_profile[0])
 	domain := ReplaceSpace(repo_to_profile[2])
 	username := ReplaceSpace(repo_to_profile[4])
-	fmt.Printf("%s//%s//%s\n", protocol, domain, username)
-	// fmt.Println(repo_to_profile[4])
+	fmt.Printf("%s//%s/%s\n", protocol, domain, username)
 }
 
 func ReplaceSpace(value string) string {
